fd42d2687fafe1ab03bfa7423603322c75d49090
Chuck McQuilkin <cmcquilkin@CMcQuilkins-MacBook-Pro-2.local>
2018-12-05 16:41:17 -0600

    Added fixed menu, Refactored into a PWA following tutorial.

---
+++ build/js/app.js
@@ -0,0 +1,57 @@
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+    return parent.appendChild(el);
+}
+
+var ul = document.getElementById('albums');
+
+fetch('https://api.myjson.com/bins/u6ixa')
+    .then(response => response.json())
+    .then(data => {
+        let albums = data;
+
+        return albums.map(function(album) {
+            let li = createNode('li');
+            var span = createNode('span');
+
+          li.innerHTML = "<a name='" + album.index + "'></a><img  width='100' height='100' src='" + album.url + "' alt='" + album.name + "' />" + album.index + '. ' + album.name;
+
+            append(span, li);
+            append(ul, li);
+
+        });
+    })
+
+function filterAlbums() {
+    var input, filter, ol, li, a, i, txtValue;
+    input = document.getElementById("filterInput");
+    filter = input.value.toUpperCase();
+    ol = document.getElementById("albums");
+    li = ol.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+function clearFilter(){
+    document.getElementById('filterInput').value = ''
+    filterAlbums();
+}
+window.onscroll = function() {scrollMenu()};
+
+function scrollMenu() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("nav").className = "fixed";
+    } else {
+        document.getElementById("nav").className = "";
+    }
+}
